f80a92b71d8bc143d131f2793f443d7c47119f36
DjXaTo <djxato@gmail.com>
2016-04-28 13:50:41 +0200

    Version 28042016 1350

---
+++ src/Controlador/Controlador.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import Vista.Interfaz;
+
+/**
+ *
+ * @author Ciclo
+ */
+class Controlador {
+
+    
+
+    Controlador(Interfaz interfaz) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void iniciar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
