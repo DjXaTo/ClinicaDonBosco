9797370fdca3586d0a83b14e0978c807ce06b7f7
Diego <Diego@DESKTOP-L28RBNP>
2016-05-05 16:54:40 +0200

    version 05052016 1654

--- src/Modelo/Modelo.java
+++ src/Modelo/Modelo.java
@@ -8,6 +8,8 @@
 package Modelo;
 
 import java.sql.*;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -17,24 +19,56 @@ public class Modelo extends Database {
     
     public void insertPaciente(String id, int nif, String aseguradora) {
         try {
-            String sql = "INSERT INTO Paciente (id, nif, aseguradora) VALUES"
+            String q = "INSERT INTO Paciente (id, nif, aseguradora) VALUES"
                     + " ('" + id + "','" + nif + "','" + aseguradora + "',juan,45678903,Adeslas)";
-            PreparedStatement pstm = this.getConexion().prepareStatement(sql);
+            PreparedStatement pstm = this.getConexion().prepareStatement(q);
             pstm.execute();
             pstm.close();
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
         
+        public DefaultTableModel tablaPacientes() {
+        DefaultTableModel tablemodel;
+        tablemodel = new DefaultTableModel();
+        try {
+            tablemodel.addColumn("id");
+            tablemodel.addColumn("nif");
+            tablemodel.addColumn("aseguradora");
+
+           
+             String q = "idPaciente, NIF, Aseguradora FROM Paciente";
+            PreparedStatement pstm = this.getConexion().prepareStatement(q);
+            ResultSet res = pstm.executeQuery();
+
+            String[] data = new String[3];
+
+            while (res.next()) {
+                data[0] = res.getString("ID");
+                data[1] = res.getString("NIF");
+                data[2] = res.getString("Aseguradora");
+                tablemodel.addRow(data);
+            }
+
+            res.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al obtener datos\n\n" + e.getMessage());
+            e.printStackTrace();
+        }
+        return tablemodel;
+    }
+}
+
+        
    
         
       
         
         
-    }
+    
     
     
    
     
     
-}
+
